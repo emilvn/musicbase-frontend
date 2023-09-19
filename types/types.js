23adef48a442cad4b11f0bccee8bdf661696781d
emilvn <emilvnielsen@hotmail.com>
2023-09-19 23:17:26 +0200

    uÃupdate types

--- types/types.js
+++ types/types.js
@@ -1,13 +1,13 @@
 /**
  * @typedef Artist
- * @property {number} id unique id of artist
+ * @property {number|null} id unique id of artist, can be null/undefined when adding new artist
  * @property {string} name name of artist
  * @property {string} image image url of artist
  * */
 
 /**
  * @typedef Album
- * @property {number} id unique id of album
+ * @property {number|null} id unique id of album can be null/undefined when adding new album
  * @property {string} name name of album
  * @property {string} image image url of album
  * @property {string[]} artist_names array of artist names
@@ -15,7 +15,7 @@
 
 /**
  * @typedef Track
- * @property {number} id id of track
+ * @property {number|null} id id of track, can be null/undefined when adding new track
  * @property {string} name name of track
  * @property {number} album_id id of album the track is on
  * */
