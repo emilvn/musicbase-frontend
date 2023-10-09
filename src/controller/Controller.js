4e6c9462c7aa6841741f2fdee816030b49b4a8ed
Bindholt <cbindholt@gmail.com>
2023-10-09 15:34:55 +0200

    implementing init, initviews

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -6,11 +6,19 @@ export class Controller{
 	#uri;
 	#container;
 	#DataService;
+	#ItemListRenderer;
 	constructor(ItemModel, uri, container) {
 		this.#ItemModel = ItemModel;
 		this.#uri = uri;
 		this.#container = container;
 		this.#DataService = new DataService(this.#uri, this.#ItemModel);
+	}
+	async init() {
+		const items = await this.#DataService.getAll()
+ 		initView(items);
+	}
+	initView(items) {
+		this.#ItemListRenderer = new ListRenderer()
 	}
 
 	
