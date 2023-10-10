2cbe822355f10bf63ac44bf6e61d425bccc2b299
emilvn <emilvnielsen@hotmail.com>
2023-10-10 10:49:26 +0200

    removed old files

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 import {setEventListeners} from "./src/controller/seteventlisteners.js";
-import {updateView} from "./src/controller/updateview.js";
-import {ListRenderer} from "./src/view/renderer/ListRenderer.js";
-import {getAlbums} from "./src/rest-service/albums.restservice.js";
-import {getTracks} from "./src/rest-service/tracks.restservice.js";
-import {getArtists} from "./src/rest-service/artists.restservice.js";
-import {AlbumRenderer} from "./src/view/renderer/AlbumRenderer.js";
-import {TrackRenderer} from "./src/view/renderer/TrackRenderer.js";
-import {ArtistRenderer} from "./src/view/renderer/ArtistRenderer.js";
+import {search} from "./src/controller/search";
 
 window.addEventListener("load", main);
 
@@ -21,24 +14,6 @@ export const endpoint = "https://musicbase-backend.azurewebsites.net";
  * @returns {Promise<void>}
  */
 async function main(){
-	setEventListeners();
-	const albums = await getAlbums()
-	const tracks = await getTracks()
-	const artist = await getArtists()
-
-	console.log(albums)
-	console.log(artist)
-	console.log(tracks)
-	try{
-		const AlbumListRenderer = new ListRenderer(albums, document.querySelector("#album-grid"), AlbumRenderer);
-		const TrackListRenderer = new ListRenderer(tracks, document.querySelector("#track-grid"), TrackRenderer);
-		const ArtistsListRenderer = new ListRenderer(artist, document.querySelector("#artist-grid"), ArtistRenderer);
-
-		AlbumListRenderer.render()
-		TrackListRenderer.render()
-		ArtistsListRenderer.render()
-	}
-	catch(err){
-		console.log(err);
-	}
+	document.querySelector("#artist-search")
+		.addEventListener("search", search);
 }
--- src/controller/seteventlisteners.js
+++
@@ -1,9 +0,0 @@
-import {search} from "./search.js";
-
-/**
- * function to set initial event listeners
- */
-export function setEventListeners(){
-	document.querySelector("#artist-search")
-		.addEventListener("search", search);
-}
