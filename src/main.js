37cfff96aaea0d545f2f92db90ee9b76845394d0
emilvn <emilvnielsen@hotmail.com>
2023-10-17 12:29:09 +0200

    made prettier spacing in main

--- src/main.js
+++ src/main.js
@@ -15,15 +15,18 @@ window.addEventListener("load", main);
  */
 async function main(){
 	TabUtility.initTabs();
+
 	const ArtistController = new Controller(Artist, "/artists", ArtistRenderer, document.querySelector("#artist-grid"));
 	const AlbumController = new Controller(Album, "/albums", AlbumRenderer, document.querySelector("#album-grid"));
 	const TrackController = new Controller(Track, "/tracks", TrackRenderer, document.querySelector("#track-grid"));
+
 	document.querySelector("#artist-search")
 		.addEventListener("search", (event) => {
 			ArtistController.search(event.target.value);
 			AlbumController.search(event.target.value);
 			TrackController.search(event.target.value);
 		});
+
 	try {
 		await ArtistController.init();
 		await AlbumController.init();
